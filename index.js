a138b71deff433ce4b746c4ffb7b5b79f3928c44
Aashish Nehete <ashnehete@gmail.com>
2017-03-28 18:23:17 +0530

    Style for article and json file

---
+++ index.js
@@ -0,0 +1,23 @@
+function openArticle(article) {
+    var link = article.dataset.target;
+    window.open(link, '_blank');
+}
+
+function loadJSON(file, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true);
+    // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback 
+            // as .open() will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+loadJSON('data.json', function(response) {
+    console.log(JSON.parse(response));
+});
